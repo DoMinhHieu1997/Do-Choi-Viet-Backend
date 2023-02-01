98881be1acbc48cdf1dcefc263e2f50759e9b6d9
vuongcdt <vuongcdt@gmail.com>
2023-02-01 18:00:27 +0700

    test db

--- routers/index.js
+++ routers/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
 const authRouter = require("./auth");
 const productRouter =  require("./product");
 
@@ -6,5 +7,10 @@ const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/products", productRouter);
+router.get('/',(_,res)=>{
+    new MongoClient("mongodb+srv://admin:admin123@cluster0.j9yngv5.mongodb.net/?retryWrites=true&w=majority").connect(()=>{
+        res.send('reconnecDB')
+    })
+})
 
 module.exports = router;
