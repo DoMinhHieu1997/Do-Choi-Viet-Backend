12993f9d5e153279938ea2a614a22f6eef943034
vuongcdt <vuongcdt@gmail.com>
2023-02-01 18:54:57 +0700

    fix db 1.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { connectToDb } = require("./database");
 const router = require("./routers");
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient } = require("mongodb");
 
 const app = express();
 app.use(
@@ -11,6 +12,11 @@ app.use(
 );
 app.use(express.json());
 
+app.use('/',(req,res,next)=>{
+    new MongoClient("mongodb+srv://admin:admin123@cluster0.j9yngv5.mongodb.net/?retryWrites=true&w=majority").connect(()=>{
+        next()
+    })
+})
 app.use(router);
 
 connectToDb();
--- routers/index.js
+++ routers/index.js
@@ -7,10 +7,5 @@ const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/products", productRouter);
-router.get('/',(req,res)=>{
-    new MongoClient("mongodb+srv://admin:admin123@cluster0.j9yngv5.mongodb.net/?retryWrites=true&w=majority").connect(()=>{
-        res.send('reconnecDB')
-    })
-})
 
 module.exports = router;
