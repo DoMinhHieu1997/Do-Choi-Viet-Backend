90eba907f43737e5e8f227ef08e603d19a9b3693
vuongcdt <vuongcdt@gmail.com>
2023-02-01 18:03:45 +0700

    fix db

--- routers/index.js
+++ routers/index.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/products", productRouter);
-router.get('/',(_,res)=>{
+router.get('/',(req,res)=>{
     new MongoClient("mongodb+srv://admin:admin123@cluster0.j9yngv5.mongodb.net/?retryWrites=true&w=majority").connect(()=>{
         res.send('reconnecDB')
     })
